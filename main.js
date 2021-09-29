6d3de63791934162fd2e6b1ce66044f48e89585d
Andrés Martínez <a.martinez@uniandes.edu.co>
2021-09-28 21:53:17 -0500

    todo menos 5 y 6

--- main.js
+++ main.js
@@ -2,50 +2,160 @@ let cart = 0
 fetch('https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json')
     .then(response => response.json())
     .then(data => {
-        list = ["Burgers", "Salads", "Tacos", "Desserts", "Drinks and Sides"]
-        for (let i = 0; i < list.length; i++) {
-            document.getElementById(list[i]).addEventListener("click", modificar(list[i], data[i]))
+        for (let i = 0; i < data.length; i++) {
+            let boton = addElement(data[i]["name"])
+            boton.addEventListener("click", () => {
+                let tituloDiv = document.getElementById("titulo")
+                tituloDiv.textContent = data[i]["name"]
+                let productosDiv = document.getElementById("productos")
+                productosDiv.textContent = ''
+                productos = data[i]["products"]
+                for (let j = 0; j < productos.length; j++) {
+                    let colDiv = document.createElement("div")
+                    colDiv.className = "col-2"
+                    let cardDiv = document.createElement("div")
+                    cardDiv.className = "card"
+                    let imgDiv = document.createElement("img")
+                    imgDiv.setAttribute("src", productos[j]["image"]);
+                    imgDiv.className = "card-img-top"
+                    let bodyDiv = document.createElement("div")
+                    bodyDiv.className = "card-body"
+                    let h5Div = document.createElement("h5")
+                    h5Div.className = "card-title"
+                    h5Div.textContent = productos[j]["name"]
+                    let pDiv = document.createElement("p")
+                    pDiv.textContent = productos[j]["description"]
+                    pDiv.className = "card-text"
+                    let sDiv = document.createElement("strong")
+                    sDiv.textContent = productos[j]["price"]
+                    let brDiv = document.createElement("br")
+                    let aDiv = document.createElement("a")
+                    aDiv.className = "btn btn-dark"
+                    aDiv.textContent = "Add to cart"
+                    productos[j]["quantity"] = 0
+                    aDiv.addEventListener('click', () => {
+                        productos[j]["quantity"] += 1
+                        cart += 1
+                        document.getElementById("items").textContent = cart + " items"
+
+                    })
+
+                    productosDiv.appendChild(colDiv)
+                    colDiv.appendChild(cardDiv)
+                    cardDiv.appendChild(imgDiv)
+                    cardDiv.appendChild(bodyDiv)
+                    bodyDiv.appendChild(h5Div)
+                    bodyDiv.appendChild(pDiv)
+                    pDiv.appendChild(brDiv)
+                    pDiv.appendChild(sDiv)
+                    bodyDiv.appendChild(aDiv)
+                }
+            })
+
+            let comprasDiv = document.getElementById("compras")
+            let crearCarrito = () => {
+                let productosDiv = document.getElementById("productos")
+                productosDiv.textContent = ''
+                productosDiv.innerHTML = '<table class="table table-striped">' +
+                    '  <thead>' +
+                    '    <tr>' +
+                    '      <th scope="col">Item</th>' +
+                    '      <th scope="col">Qty.</th>' +
+                    '      <th scope="col">Description</th>' +
+                    '      <th scope="col">Unit Price</th>' +
+                    '      <th scope="col">Amount</th>' +
+                    '      <th scope="col">Modify</th>' +
+                    '    </tr>' +
+                    '  </thead>' +
+                    '  <tbody id="tbody">' +
+                    '  </tbody>' +
+                    '</table>'
+                let k = 1
+                let total = 0
+                let tabla = document.getElementById("tbody")
+                for (let ii = 0; ii < data.length; ii++) {
+                    productos = data[ii]["products"]
+                    for (let jj = 0; jj < productos.length; jj++) {
+                        let quantity = productos[jj]["quantity"]
+                        if (quantity > 0) {
+                            let subT = productos[jj]["price"] * quantity
+                            let row = '    <th scope="row">' +
+                                k +
+                                '    </th>' +
+                                '    <td>' +
+                                quantity +
+                                '    </td>' +
+                                '    <td>' +
+                                productos[jj]["name"] +
+                                '    </td>' +
+                                '    <td>' +
+                                productos[jj]["price"] +
+                                '    </td>' +
+                                '    <td>' +
+                                Math.round(subT * 100) / 100 +
+                                '    </td>' +
+                                '    <td>' +
+                                '    <button type="button" class="btn btn-dark" id="a">-</button>' +
+                                '    <button type="button" class="btn btn-dark" id="b">+</button>' +
+                                '    </td>'
+                            text = document.createElement('tr')
+                            text.innerHTML = row
+                            tabla.appendChild(text)
+                            k += 1
+                            total += subT
+                            let botonA = document.getElementById("a")
+                            botonA.addEventListener('click', () => {
+                                productos[jj]["quantity"] -= 1
+                                cart -= 1
+                                document.getElementById("items").textContent = cart + " items"
+                                crearCarrito()
+                            })
+                            botonA.removeAttribute("id")
+                            let botonB = document.getElementById("b")
+                            botonB.addEventListener('click', () => {
+                                productos[jj]["quantity"] += 1
+                                cart += 1
+                                document.getElementById("items").textContent = cart + " items"
+                                crearCarrito()
+                            })
+                            botonB.removeAttribute("id")
+                        }
+                    }
+                }
+                let row = '   <div class="col-sm">' +
+                    '<p>' +
+                    '<strong>Total $' + Math.round(total * 100) / 100 + '</strong>' +
+                    '</p>' +
+                    '</div>' +
+                    '<div class="col-sm">' +
+                    '<button type="button" class="btn btn-success total" >Confirm order</button>' +
+                    '<button type="button" class="btn btn-danger total">Cancel</button>' +
+                    '</div>'
+
+                let text1 = document.createElement('div')
+                text1.className = "container-fluid"
+                text = document.createElement('div')
+                text.className = "row"
+                text.innerHTML = row
+                text1.appendChild(text)
+                productosDiv.appendChild(text1)
+
+                let tituloDiv = document.getElementById("titulo")
+                tituloDiv.textContent = 'Order detail'
+            }
+            comprasDiv.addEventListener('click', crearCarrito)
+
         }
     });
 
-
-function modificar(nombre, productos) {
-    let tituloDiv = document.getElementById("titulo")
-    tituloDiv.textContent = nombre
-    let productosDiv = document.getElementById("productos")
-    productosDiv.textContent = ''
-    for (var p in productos["products"]) {
-        addProduct(p["name"], p["image"], p["description"], p["price"], productosDiv)
-    }
-
-    console.log(nombre)
-}
-
-function addProduct(nombre, image, desc, price, parent) {
-    let colDiv = document.createElement("div")
-    colDiv.className = "col"
-    let cardDiv = document.createElement("div")
-    cardDiv.className = "card"
-    let imgDiv = document.createElement("img")
-    imgDiv.setAttribute("src", image);
-    imgDiv.className = "card-img-top"
-    let bodyDiv = document.createElement("div")
-    bodyDiv.className = "card-body"
-    let h5Div = document.createElement("h5")
-    h5Div.className = "card-title"
-    h5Div.textContent = nombre
-    let pDiv = document.createElement("p")
-    pDiv.textContent = desc
-    pDiv.className = "card-text"
+function addElement(nombre) {
+    let liDiv = document.createElement("li")
+    liDiv.className = "nav-item"
     let aDiv = document.createElement("a")
-    aDiv.className = "btn btn-primary"
-    aDiv.textContent = price
-
-    parent.appendChild(colDiv)
-    colDiv.appendChild(cardDiv)
-    cardDiv.appendChild(imgDiv)
-    cardDiv.appendChild(bodyDiv)
-    bodyDiv.appendChild(h5Div)
-    bodyDiv.appendChild(pDiv)
-    bodyDiv.appendChild(aDiv)
+    aDiv.className = "nav-link"
+    aDiv.textContent = nombre
+    let ulDiv = document.getElementById("navbarElemnts")
+    liDiv.appendChild(aDiv)
+    ulDiv.appendChild(liDiv)
+    return aDiv
 }
