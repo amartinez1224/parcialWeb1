e9ab1eb054ce9bcd7bdc23e2c304a6dd730a6193
Andrés Martínez <a.martinez@uniandes.edu.co>
2021-09-29 21:37:12 -0500

    version final

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let cart = 0
+let modal = new bootstrap.Modal(document.getElementById("exampleModal"), {})
 fetch('https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json')
     .then(response => response.json())
     .then(data => {
@@ -32,14 +33,17 @@ fetch('https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173da
                     let aDiv = document.createElement("a")
                     aDiv.className = "btn btn-dark"
                     aDiv.textContent = "Add to cart"
-                    productos[j]["quantity"] = 0
+                    if (!("quantity" in productos[j])) productos[j]["quantity"] = 0
                     aDiv.addEventListener('click', () => {
                         productos[j]["quantity"] += 1
                         cart += 1
                         document.getElementById("items").textContent = cart + " items"
 
                     })
-
+                    console.log({
+                        "p": productos[j]["name"],
+                        "c": productos[j]["quantity"]
+                    })
                     productosDiv.appendChild(colDiv)
                     colDiv.appendChild(cardDiv)
                     cardDiv.appendChild(imgDiv)
@@ -51,101 +55,144 @@ fetch('https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173da
                     bodyDiv.appendChild(aDiv)
                 }
             })
+        }
 
-            let comprasDiv = document.getElementById("compras")
-            let crearCarrito = () => {
-                let productosDiv = document.getElementById("productos")
-                productosDiv.textContent = ''
-                productosDiv.innerHTML = '<table class="table table-striped">' +
-                    '  <thead>' +
-                    '    <tr>' +
-                    '      <th scope="col">Item</th>' +
-                    '      <th scope="col">Qty.</th>' +
-                    '      <th scope="col">Description</th>' +
-                    '      <th scope="col">Unit Price</th>' +
-                    '      <th scope="col">Amount</th>' +
-                    '      <th scope="col">Modify</th>' +
-                    '    </tr>' +
-                    '  </thead>' +
-                    '  <tbody id="tbody">' +
-                    '  </tbody>' +
-                    '</table>'
-                let k = 1
-                let total = 0
-                let tabla = document.getElementById("tbody")
+        let comprasDiv = document.getElementById("compras")
+        let crearCarrito = () => {
+            let productosDiv = document.getElementById("productos")
+            productosDiv.textContent = ''
+            productosDiv.innerHTML = '<table class="table table-striped">' +
+                '  <thead>' +
+                '    <tr>' +
+                '      <th scope="col">Item</th>' +
+                '      <th scope="col">Qty.</th>' +
+                '      <th scope="col">Description</th>' +
+                '      <th scope="col">Unit Price</th>' +
+                '      <th scope="col">Amount</th>' +
+                '      <th scope="col">Modify</th>' +
+                '    </tr>' +
+                '  </thead>' +
+                '  <tbody id="tbody">' +
+                '  </tbody>' +
+                '</table>'
+            let k = 1
+            let total = 0
+            let tabla = document.getElementById("tbody")
+            for (let ii = 0; ii < data.length; ii++) {
+                let productoss = data[ii]["products"]
+                for (let jj = 0; jj < productoss.length; jj++) {
+                    let quantity = productoss[jj]["quantity"]
+                    if (quantity > 0) {
+                        let subT = productoss[jj]["price"] * quantity
+                        let row = '    <th scope="row">' +
+                            k +
+                            '    </th>' +
+                            '    <td>' +
+                            quantity +
+                            '    </td>' +
+                            '    <td>' +
+                            productoss[jj]["name"] +
+                            '    </td>' +
+                            '    <td>' +
+                            productoss[jj]["price"] +
+                            '    </td>' +
+                            '    <td>' +
+                            Math.round(subT * 100) / 100 +
+                            '    </td>' +
+                            '    <td>' +
+                            '    <button type="button" class="btn btn-dark" id="a">-</button>' +
+                            '    <button type="button" class="btn btn-dark" id="b">+</button>' +
+                            '    </td>'
+                        text = document.createElement('tr')
+                        text.innerHTML = row
+                        tabla.appendChild(text)
+                        k += 1
+                        total += subT
+                        let botonA = document.getElementById("a")
+                        botonA.addEventListener('click', () => {
+                            productoss[jj]["quantity"] -= 1
+                            cart -= 1
+                            document.getElementById("items").textContent = cart + " items"
+                            crearCarrito()
+                        })
+                        botonA.removeAttribute("id")
+                        let botonB = document.getElementById("b")
+                        botonB.addEventListener('click', () => {
+                            productoss[jj]["quantity"] += 1
+                            cart += 1
+                            document.getElementById("items").textContent = cart + " items"
+                            crearCarrito()
+                        })
+                        botonB.removeAttribute("id")
+                    }
+                }
+            }
+            let row = '   <div class="col-sm">' +
+                '<p>' +
+                '<strong>Total $' + Math.round(total * 100) / 100 + '</strong>' +
+                '</p>' +
+                '</div>' +
+                '<div class="col-sm">' +
+                '<button type="button" class="btn btn-success total" id="a">Confirm order</button>' +
+                '<button type="button" class="btn btn-danger total" id="b">Cancel</button>' +
+                '</div>'
+
+            let text1 = document.createElement('div')
+            text1.className = "container-fluid"
+            text = document.createElement('div')
+            text.className = "row"
+            text.innerHTML = row
+            text1.appendChild(text)
+            productosDiv.appendChild(text1)
+
+            let botonA = document.getElementById("a")
+            botonA.addEventListener('click', () => {
+                printList = []
+                k = 1
                 for (let ii = 0; ii < data.length; ii++) {
-                    productos = data[ii]["products"]
-                    for (let jj = 0; jj < productos.length; jj++) {
-                        let quantity = productos[jj]["quantity"]
+                    productoss = data[ii]["products"]
+                    for (let jj = 0; jj < productoss.length; jj++) {
+                        let quantity = productoss[jj]["quantity"]
                         if (quantity > 0) {
-                            let subT = productos[jj]["price"] * quantity
-                            let row = '    <th scope="row">' +
-                                k +
-                                '    </th>' +
-                                '    <td>' +
-                                quantity +
-                                '    </td>' +
-                                '    <td>' +
-                                productos[jj]["name"] +
-                                '    </td>' +
-                                '    <td>' +
-                                productos[jj]["price"] +
-                                '    </td>' +
-                                '    <td>' +
-                                Math.round(subT * 100) / 100 +
-                                '    </td>' +
-                                '    <td>' +
-                                '    <button type="button" class="btn btn-dark" id="a">-</button>' +
-                                '    <button type="button" class="btn btn-dark" id="b">+</button>' +
-                                '    </td>'
-                            text = document.createElement('tr')
-                            text.innerHTML = row
-                            tabla.appendChild(text)
+                            printList.push({
+                                "item": k,
+                                "quantity": quantity,
+                                "description": productoss[jj]["name"],
+                                "unitPrice": productoss[jj]["price"]
+                            })
                             k += 1
-                            total += subT
-                            let botonA = document.getElementById("a")
-                            botonA.addEventListener('click', () => {
-                                productos[jj]["quantity"] -= 1
-                                cart -= 1
-                                document.getElementById("items").textContent = cart + " items"
-                                crearCarrito()
-                            })
-                            botonA.removeAttribute("id")
-                            let botonB = document.getElementById("b")
-                            botonB.addEventListener('click', () => {
-                                productos[jj]["quantity"] += 1
-                                cart += 1
-                                document.getElementById("items").textContent = cart + " items"
-                                crearCarrito()
-                            })
-                            botonB.removeAttribute("id")
                         }
                     }
                 }
-                let row = '   <div class="col-sm">' +
-                    '<p>' +
-                    '<strong>Total $' + Math.round(total * 100) / 100 + '</strong>' +
-                    '</p>' +
-                    '</div>' +
-                    '<div class="col-sm">' +
-                    '<button type="button" class="btn btn-success total" >Confirm order</button>' +
-                    '<button type="button" class="btn btn-danger total">Cancel</button>' +
-                    '</div>'
+                console.log(printList)
+            })
+            botonA.removeAttribute("id")
+            let botonB = document.getElementById("b")
+            botonB.addEventListener('click', () => {
+                modal.show()
+            })
+            botonB.removeAttribute("id")
 
-                let text1 = document.createElement('div')
-                text1.className = "container-fluid"
-                text = document.createElement('div')
-                text.className = "row"
-                text.innerHTML = row
-                text1.appendChild(text)
-                productosDiv.appendChild(text1)
-
-                let tituloDiv = document.getElementById("titulo")
-                tituloDiv.textContent = 'Order detail'
-            }
-            comprasDiv.addEventListener('click', crearCarrito)
-
+            let tituloDiv = document.getElementById("titulo")
+            tituloDiv.textContent = 'Order detail'
         }
+        comprasDiv.addEventListener('click', crearCarrito)
+
+        let vaciar = document.getElementById("modalRefresh")
+        vaciar.addEventListener('click', ()=>{
+            for (let i = 0; i < data.length; i++){
+                productos = data[i]["products"]
+                for (let j = 0; j < productos.length; j++) {
+                    productos[j]["quantity"] = 0
+                }
+            }
+            cart = 0
+            document.getElementById("items").textContent = cart + " items"
+            crearCarrito()
+            modal.hide()
+        })
+
+
     });
 
 function addElement(nombre) {
